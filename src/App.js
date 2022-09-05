9c9eaa5c8ed056a319bc55e8c077b3f9c0ae695e
Zuka-Dev <danazuka2003@gmail.com>
2022-09-05 12:53:04 +0100

    Message

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Home from "./pages/Home";
 import MovieDetails from "./pages/MovieDetails";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} exact />
-        <Route path="/:id" element={<MovieDetails />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} exact />
+                <Route path="/:id" element={<MovieDetails />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/request.js
+++ src/request.js
@@ -1,13 +1,13 @@
+export const REACT_APP_API_KEY = "6775df9b7d0c9d847f733217bec9d75a";
+const request = {
+  fetchTrending: `/trending/all/week?api_key=${REACT_APP_API_KEY}&language=en-US`,
+  fetchNetfixOriginals: `/discover/tv?api_key=${REACT_APP_API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${REACT_APP_API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${REACT_APP_API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${REACT_APP_API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${REACT_APP_API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${REACT_APP_API_KEY}&with_genres=10749`,
+  fetchDocumentries: `/discover/movie?api_key=${REACT_APP_API_KEY}&with_genres=99`,
+};
 
- const request = {
-    fetchTrending: `/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`,
-    fetchNetfixOriginals: `/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=10749`,
-    fetchDocumentries: `/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=99`,
-}
-
-export default request
+export default request;
